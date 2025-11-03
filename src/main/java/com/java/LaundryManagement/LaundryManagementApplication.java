7a27d64747cfaa548cffb557c33d2103a7ca0bce
chiku0989 <neelv448@gmail.com>
2025-11-03 18:33:44 +0530

    feat : Added initial project boilerplate

---
+++ src/main/java/com/java/LaundryManagement/LaundryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.java.LaundryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryManagementApplication.class, args);
+	}
+
+}
